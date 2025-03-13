e2268d13c1a51146a3cecd6c650f6a7cd23d531d
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-03-13 07:37:51 +0000

    adding files

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -89,4 +89,4 @@ output "internet_gateway_id" {
 
 output "nat_gateway_id" {
   value = aws_nat_gateway.this.id
-}SSS
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "fargate-state-bucket-uc3"
+    key            = "terraform"
+    region         = "eu-west-2"
+    encrypt        = true
+  }
+}
